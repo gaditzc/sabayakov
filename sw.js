876ae57250ca9aee3fa28af83e8b011c32b036ff
gaditzc <gaditzc@gmail.com>
2026-05-17 00:31:41 +0300

    Implement Amazing Race game app from config

--- sw.js
+++ sw.js
@@ -1,7 +1,71 @@
-self.addEventListener("install", function () {
+const CACHE_NAME = "amazing-race-shell-v1";
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.json",
+  "./config.json",
+  "./assets/icon-192.svg",
+  "./assets/icon-512.svg",
+];
+
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(APP_SHELL);
+    }),
+  );
   self.skipWaiting();
 });
 
 self.addEventListener("activate", function (event) {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames.map(function (cacheName) {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+          return null;
+        }),
+      ).then(function () {
+        return self.clients.claim();
+      });
+    }),
+  );
+});
+
+self.addEventListener("fetch", function (event) {
+  if (event.request.method !== "GET") {
+    return;
+  }
+
+  const requestUrl = new URL(event.request.url);
+  if (requestUrl.origin !== self.location.origin) {
+    return;
+  }
+
+  if (requestUrl.pathname.endsWith("/config.json")) {
+    event.respondWith(
+      fetch(event.request)
+        .then(function (response) {
+          const responseClone = response.clone();
+          caches.open(CACHE_NAME).then(function (cache) {
+            cache.put(event.request, responseClone);
+          });
+          return response;
+        })
+        .catch(function () {
+          return caches.match(event.request);
+        }),
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(function (cachedResponse) {
+      return cachedResponse || fetch(event.request);
+    }),
+  );
 });
